f68806144820415ae3e124e93e1dd733540fd41f
EliasSvedberg <51356784+EliasSvedberg@users.noreply.github.com>
2021-03-30 20:49:57 +0200

    test

---
+++ script.js
@@ -0,0 +1,8 @@
+// function submitOnEnter(event){
+//     if(event.which === 13){
+//         event.target.form.dispatchEvent(new Event("submit", {cancelable: true}));
+//         event.preventDefault(); // Prevents the addition of a new line in the text field (not needed in a lot of cases)
+//     }
+// }
+//
+// document.getElementById("usermsg").addEventListener("keypress", submitOnEnter);
