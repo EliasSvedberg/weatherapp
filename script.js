6af923155f19dc6928d3b7c26d27adc29f4f198e
EliasSvedberg <51356784+EliasSvedberg@users.noreply.github.com>
2021-04-03 21:41:59 +0200

    added most of the functionality

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
-// function submitOnEnter(event){
-//     if(event.which === 13){
-//         event.target.form.dispatchEvent(new Event("submit", {cancelable: true}));
-//         event.preventDefault(); // Prevents the addition of a new line in the text field (not needed in a lot of cases)
-//     }
-// }
-//
-// document.getElementById("usermsg").addEventListener("keypress", submitOnEnter);
+class WeatherApp {
+  constructor() {
+    this.inputContent = document.querySelector('.user-input')
+    this.citynamediv = document.querySelector('.city-name')
+    this.dayAndTime = document.querySelector('.day-time')
+    this.weatherDescription = document.querySelector('.weather-description')
+    this.temperature = document.querySelector('.temperature')
+    this.createEventListener()
+  }
+
+  fetchWeather() {
+    var cityName = this.inputContent.value
+    console.log(cityName)
+    this.url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
+    fetch(this.url)
+    .then(response => response.json())
+    .then(data => {
+      this.citynamediv.textContent = data.name
+      this.dayAndTime.textContent = data.dt
+      this.weatherDescription.textContent = data.weather[0].description
+      this.temperature.textContent = data.main.temp
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
+  createEventListener() {
+    this.inputContent.addEventListener('keypress', (e) => {
+      if (e.key === 'Enter') {
+        this.fetchWeather()
+      }
+    })
+  }
+}
+
+
+
+const weatherApp = new WeatherApp()
+
+const hej = document.querySelector('.user-input')
